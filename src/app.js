097c0797dcbb34c31e19f3adb5a0e995d4e68846
meta-434 <ach1@vt.edu>
2020-05-15 05:10:13 -0400

    authentication wip 1
    add jwt token creation (static user / pass)

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const config = require('./config');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
@@ -9,15 +12,57 @@ const notesRouter = require('./notes/notes-router');
 
 const app = express();
 
+// set secret
+app.set('Secret', config.SECRET_KEY);
+
+// log requests
 app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
     skip: () => NODE_ENV === 'test'
 }));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// cors and header obfuscation
 app.use(cors());
 app.use(helmet());
 
+// routers
 app.use('/api/sessions', sessionsRouter);
 app.use('/api/notes', notesRouter);
 
+app.post('/api/authenticate', (req, res) => {
+    if (req.body.username ==='testuser') {
+        if (req.body.password === 123) {
+            // everything ok, create token
+            const payload = {check: true};
+
+            let token = jwt.sign(payload, app.get('Secret'), {
+                expiresIn: 1440 //expires in 24 hours
+            });
+
+            res.json({
+                message: 'authentication done',
+                token: token
+            });
+        } else {
+            res.json({message:"please check your password !"});
+        }
+    } else {
+        res.json({message:"user not found !"});
+    }
+})
+
+
+// '/' catcher
+app.get('/', function(req, res) {
+    res.send(`Hello world, I'm running on http://localhost:${process.env.PORT}`)
+})
+
+// error handling
 app.use(function errorHandler(error, req, res, next) {
     let response;
     if (NODE_ENV === 'production') {
--- src/config.js
+++ src/config.js
@@ -3,4 +3,5 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/capstone_one',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/capstone_one_test',
+    SECRET_KEY: process.env.SECRET_KEY || 'change-this-default-key'
 };
