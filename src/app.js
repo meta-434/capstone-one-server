7cb725b5bf5253cf2810621d069f81781b9c93f2
meta-434 <ach1@vt.edu>
2020-05-18 23:30:06 -0400

    set new headers and allow preflight
    fix cors, finally, hopefully

--- src/app.js
+++ src/app.js
@@ -21,7 +21,17 @@ app.set('Secret', config.SECRET_KEY);
 app.use(helmet());
 
 // cors
-app.use(cors());
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    // allow preflight
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
 
 // use morgan to log requests to the console
 app.use(morgan('dev'));
